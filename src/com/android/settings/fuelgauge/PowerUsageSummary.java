25b55dcaa68199f17178b4654ada9d3ef2476c69
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-09-18 17:17:30 +0200

    Battery fragment: move battery graph to first fragment
    -Fix needed: layout seems to ignore marginStart
    
    Change-Id: Ie6e91efc314e195ca367b1dcf65330898a805168

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -53,6 +53,9 @@ import com.android.settingslib.utils.PowerUtil;
 import com.android.settingslib.utils.StringUtil;
 import com.android.settingslib.widget.LayoutPreference;
 
+import com.android.settings.widget.UsageView;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -120,6 +123,13 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                     mBatteryHeaderPreferenceController.updateHeaderPreference(batteryInfo);
                     mBatteryInfo = batteryInfo;
                     updateLastFullChargePreference();
+                    Handler h = new Handler();
+                        h.postDelayed(() -> {
+                            UsageView usageView = (UsageView) mBatteryLayoutPref.findViewById(R.id.battery_usage);
+                            usageView.findViewById(R.id.label_group).setAlpha(.7f);
+                            mBatteryInfo.bindHistory(usageView);
+                        }, 300);
+
                 }
 
                 @Override
